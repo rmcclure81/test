4578e17c16050eb06ca87810c08c9d68d8656761
Robert McClure <robertmcclure@Roberts-MacBook-Pro.local>
2020-08-29 12:34:05 -0600

    Files commited using automated method

--- javascript_files/nav2.js
+++ javascript_files/nav2.js
@@ -3,7 +3,7 @@ document.write('<div class="dropdown" id="home">');
 document.write(
   '<button class="btn_dropdown_toggle" type="button" data-toggle="dropdown">'
 );
-document.write('<a href="index.html" class="links">Home</a>');
+document.write('<a href="/html/index.html" class="links">Home</a>');
 document.write('</button>');
 document.write('</div>');
 
@@ -14,6 +14,38 @@ document.write(
 );
 document.write(
   '<span>Week #1<br /> (XML Files) &nbsp;<span class="caret">▼</span></span>'
+);
+document.write('</button>');
+document.write('<div class="dropdownContent">');
+document.write(
+  '<a href="../xml_data_files/art-plain.xml" class="links">Art Collection</a>'
+);
+document.write('<br /><br />');
+document.write(
+  '<a href="../xml_data_files/verses-plain.xml" class="links">Bible Verse Collection</a>'
+);
+document.write('<br /><br />');
+document.write(
+  '<a href="../xml_data_files/family-plain.xml" class="links">Family Collection</a>'
+);
+document.write('<br /><br />');
+document.write(
+  '<a href="../xml_data_files/movies-plain.xml" class="links">Movie Collection</a>'
+);
+document.write('<br /><br />');
+document.write(
+  '<a href="../xml_data_files/music-plain.xml" class="links">Music Collection</a>'
+);
+document.write('</div>');
+document.write('</div>');
+
+//** Week 2 **//
+document.write('<div class="dropdown" id="w2">');
+document.write(
+  '<button class="btn_dropdown_toggle" type="button" data-toggle="dropdown">'
+);
+document.write(
+  '<span>Week #2<br /> (XSLT Files) &nbsp;<span class="caret">▼</span></span>'
 );
 document.write('</button>');
 document.write('<div class="dropdownContent">');
@@ -35,38 +67,6 @@ document.write(
 document.write('<br /><br />');
 document.write(
   '<a href="../xml_data_files/music.xml" class="links">Music Collection</a>'
-);
-document.write('</div>');
-document.write('</div>');
-
-//** Week 2 **//
-document.write('<div class="dropdown" id="w2">');
-document.write(
-  '<button class="btn_dropdown_toggle" type="button" data-toggle="dropdown">'
-);
-document.write(
-  '<span>Week #2<br /> (XSLT Files) &nbsp;<span class="caret">▼</span></span>'
-);
-document.write('</button>');
-document.write('<div class="dropdownContent">');
-document.write(
-  '<a href="../xml_data_files/art2.xml" class="links">Art Collection</a>'
-);
-document.write('<br /><br />');
-document.write(
-  '<a href="../xml_data_files/verses2.xml" class="links">Bible Verse Collection</a>'
-);
-document.write('<br /><br />');
-document.write(
-  '<a href="../xml_data_files/family2.xml" class="links">Family Collection</a>'
-);
-document.write('<br /><br />');
-document.write(
-  '<a href="../xml_data_files/movies2.xml" class="links">Movie Collection</a>'
-);
-document.write('<br /><br />');
-document.write(
-  '<a href="../xml_data_files/music2.xml" class="links">Music Collection</a>'
 );
 document.write('</div>');
 document.write('</div>');
